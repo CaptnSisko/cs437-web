e35e859e10e65190aea1f6ec445dbed080fc106e
John M. Wolff <jmwolff3@users.noreply.github.com>
2022-05-14 23:21:30 -0500

    John (#6)
    * release ready
    
    * typo
    
    * remove useless
    
    * time
    
    * add 2 oz buffer
    
    * remove unused imports
    
    * account for refilling bottle

---
+++ iot-device/water_bottle.py
@@ -0,0 +1,79 @@
+from sense_hat import SenseHat
+from ADCDevice import *
+import json
+from datetime import datetime
+import math
+import requests
+
+import smbus
+import time
+
+ENDPOINT = "https://cs437.twong.dev/api/send"
+
+RADIUS = 1.175
+HEIGHT = 9.4
+FULL = 174 # voltage = 2.25
+EMPTY = 200 # voltage = 2.58
+MAX_VOL = 22
+MIN_FILL_DETECT = 10
+
+water_volume = 0
+
+def send_to_server(water_consumed, temp, humid):
+    response = requests.post(ENDPOINT, data = {'waterLevel': water_consumed, 'temperature': temp, 'humidity': humid, 'timestamp': datetime.now()})
+    return
+
+def get_current_volume(adc):
+    samples = [adc.analogRead(0) for x in range(10)]
+    average = sum(samples)/len(samples)
+
+    percent = (EMPTY-average)/(EMPTY-FULL)
+    liquid_level = HEIGHT*percent
+    volume = math.pi*math.pow(RADIUS, 2)*liquid_level*0.554113
+    return volume
+
+def measure_water(adc, sense):
+    measured_water_volume = get_current_volume(adc)
+
+    global water_volume
+
+    if measured_water_volume+2 < water_volume:
+        t = sense.get_temperature()
+        h = sense.get_humidity()
+        t = round(t, 2)
+        h = round(h, 2)
+        difference = round(water_volume - measured_water_volume, 2)
+        print("Difference in volume detected {}".format(difference))
+        water_volume = measured_water_volume
+        send_to_server(difference, t, h)
+    elif measured_water_volume > water_volume + MIN_FILL_DETECT:
+        water_volume = MAX_VOL
+
+def main():
+    sense = SenseHat()
+    sense.set_imu_config(False, True, True)
+    adc = ADCDevice()
+    adc = ADS7830()
+
+    global water_volume
+    water_volume = get_current_volume(adc)
+
+    while True:
+        acceleration = sense.get_accelerometer_raw()
+        x = acceleration['x']
+        y = acceleration['y']
+        z = acceleration['z']
+
+        x = abs(x)
+        y = abs(y)
+        z = abs(z)
+
+        if x > 1 or y > 1 or z > 1:
+            print("Motion Detected")
+            time.sleep(25)
+            measure_water(adc, sense)
+        else:
+            sense.clear()
+
+if __name__ == '__main__':
+    main()
